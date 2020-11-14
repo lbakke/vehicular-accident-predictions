bfe665ca6880c71cb614b7bd60bc1091d16b5db1
lbakke <lbakke@nd.edu>
2020-11-13 20:45:53 -0500

    code for compiling datasets

--- compile_data.py
+++ compile_data.py
@@ -98,33 +98,67 @@ df = df.join(popByZip, on="Zipcode")
 for feature in feature_names: 
     df = df[pd.notnull(df[feature])]
 
+df = df.sample(frac=1)
+
+def classify(num):
+    if num == 1 or num == 2:
+        return 0
+    else:
+        return 1
+
+max_values = 200000
+df["Severity"] = df["Severity"].map(classify)
+
+''' UNCOMMENT THE NEXT TWO LINES TO ADD UNDER SAMPLING '''
+# df = df.sort_values(by=["Severity"])
+# df = df.iloc[273000:]
+
+print(df)
+
+print(f"Severity 0: {len(df[df['Severity'] == 0])}")
+print(f"Severity 1: {len(df[df['Severity'] == 1])}")
+
 #FIXME: add df.to_csv -> full_data_10_25.csv
 
 # ''' begin k-fold validation technique '''
 # #1 shuffle dataset randomly
-# df = df.sample(frac=1)
+df = df.sample(frac=1)
+
+# copy = df.copy()
+# # Sample 80% of dataframe for training
+# # The random_state paramter means there is a different random split each run
+# train = copy.sample(frac=0.8, random_state=0) 
+# test_and_valid = copy.drop(train.index) 
+# # Sample half of the remaining 20% for testing and half for validation (10% of original df each)
+# test = test_and_valid.sample(frac=0.5, random_state=0)
+# valid = test_and_valid.drop(test.index)
+
+# Write groups to their respective files
+# train.to_csv('./10_25_data/training_data_10_26.csv')
+# test.to_csv('./10_25_data/testing_data_10_26.csv')
+# valid.to_csv('./10_25_data/validation_data_10_26.csv')
 
 # #2 split into 10 groups of equal size
-# length = len(df.index)
-# group_size = int(length/10)
-# prev = 0
+length = len(df.index)
+group_size = int(length/10)
+prev = 0
 
 # #3 use 8 of these groups for training, 1 for testing, and 1 for validation
 # # write directly to their respective files 
 
 # # try 70% training and 30% testing
 
-# end = group_size * 8
-# newdf = df.iloc[prev:end]
-# prev = end + 1
-# newdf.to_csv('training_data_10_25.csv')
+end = group_size * 8
+newdf = df.iloc[prev:end]
+prev = end + 1
+newdf.to_csv('training_data_10_28.csv')
 
-# end = 9 * group_size
-# newdf = df.iloc[prev:end]
-# prev = end + 1
-# newdf.to_csv('testing_data_10_25.csv')
+end = 9 * group_size
+newdf = df.iloc[prev:end]
+prev = end + 1
+newdf.to_csv('testing_data_10_28.csv')
 
-# end = 10 * group_size
-# newdf = df.iloc[prev:end]
-# prev = end + 1
-# newdf.to_csv('validation_data_10_25.csv')
+end = 10 * group_size
+newdf = df.iloc[prev:end]
+prev = end + 1
+newdf.to_csv('validation_data_10_28.csv')
